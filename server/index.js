bf0310521be165a54e701e2969f3058fe2cf2926
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-08 23:52:02 +0530

    initlaized routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// LOAD DEPENDENCIES ----------------------------------------------------
 // load express 
 const express = require('express')
 
@@ -7,11 +8,40 @@ const app = express()
 // load cors 
 const cors = require('cors')
 
-// middleware
+// load db connect
+const pool = require('./db')
+
+
+// MIDDLEWARE ------------------------------------------------------------
 // use cors in app 
 app.use(cors())
 app.use(express.json()) // get only body of request from client side  
 
+// ROUTES ------------------------------------------------------------
+// create a new todo 
+
+app.post("/todos", async (req,res) => {
+
+})
+
+// get all todos
+
+app.get("/todos", async (req,res) => {
+
+})
+
+// update a todo
+
+app.put("/todos/:id", async (req,res) => {
+
+})
+
+// delete a todo
+app.delete("/todos/:id", async (req,res) => {
+
+})
+
+// SERVER LISTEN ---------------------------------------------------------
 // start server listen 
 app.listen(5000, '0.0.0.0',()=>{
     console.log('NodeJS server started on port 3000')
